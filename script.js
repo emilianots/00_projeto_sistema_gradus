d7e101de8e42e41ca547d3738955d2af3819039f
emiliano <emiliano@alu.ufc.br>
2018-11-23 14:55:53 -0300

    primeiras adições no menu lateral e adição do framework LESS

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function store(nome, pass) {
 //interação com o botão de menu
 $("#menu-buttom").click(function () {
     $("#nav-menu").addClass("nav-show");
+    $(".overlay").fadeIn(200);
 })
 $("#app").mouseup(function (e) {
     var menu = $("#nav-menu");
     if ((!menu.is(e.target)) && (menu.has(e.target).length === 0)) {
         $("#nav-menu").removeClass("nav-show");
+        $(".overlay").fadeOut(200);
     }
 })
