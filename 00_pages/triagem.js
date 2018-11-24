8b518a7faa870e1e1796e368635b044e85ed66a8
emiliano <emiliano@alu.ufc.br>
2018-11-24 16:04:26 -0300

    menores mudanças adicionadas - interação com servidor rest e geração de lista de pacientes

--- 00_pages/triagem.js
+++ 00_pages/triagem.js
@@ -8,15 +8,33 @@ function getPacientes() {
         async: false,
         success: function (data) {
             pacientes = data
+            pacientes.sort(sortByAtribute('nome'));
         }
     })
 }
+function createList(lista) {
+    for (let elem of lista) {
+        let li = document.createElement("li");
+        li.classList.add("list-group-item","list-group-item-action");
+        li.innerHTML = elem.nome;
+        $("#list-pacientes").append(li);
+    }
+}
+var sortByAtribute = function (prop) {
+    return function (x, y) {
+        return ((x[prop] === y[prop]) ? 0 : ((x[prop]) > (y[prop]) ? 1 : -1));
+    }
+}
 
 getPacientes();
+
+//console.table(pacientes);
+
 $("#main-op-paciente-internado").click(function () {
-    console.table(pacientes);
+    //console.table(pacientes);
     $("#sis-body-panel").hide();
     $("#step-back").show();
+    createList(pacientes);
     $("#list-pacientes").show();
 })
 
