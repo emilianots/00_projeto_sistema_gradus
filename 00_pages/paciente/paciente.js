1fabbdf4a282224aa203b63df5fd9b2a68ebc4bd
emiliano <emiliano@alu.ufc.br>
2018-11-25 22:41:12 -0300

    maiores adições - inicio do painel do paciente e testes com geração de arquivos em PDF; preenchimento dinâmico dos dados dos pacientes no painel

--- 00_pages/paciente/paciente.js
+++ 00_pages/paciente/paciente.js
@@ -1,3 +1,36 @@
 
 var paciente = JSON.parse(localStorage.getItem('paciente'));
+window.onload = function () {
+    if (paciente == null) {
+        return;
+    }
+    $("#nome-paciente").append(paciente.nome);
+    $("#sexo").append(paciente.sexo);
+    $("#data-nasc").append(paciente.dataNasc);
+    $("#rg").append(paciente.rg);
+    $("#naturalidade").append(paciente.naturalidade);
+    $("#nome-mae").append(paciente.nomeMae);
+    $("#nome-pai").append(paciente.nomePai);
+    $("#std-civil").append(paciente.estadoCivil);
+    $("#nome-conjuge").append(paciente.nomeConjugue);
+    $("#profissao").append(paciente.profissão);
 
+}
+function gerarPDF() {
+    $("#app").css({
+        width: '100vw',
+        border: '1px, solid gray'
+    })
+    $("#step-back").css('display', 'none');
+    $("#rodape").css("display", "none");
+    
+    window.print();
+    $("#app").css({
+        width: '550px',
+        border: '0px, solid transparent'
+    })
+
+    $("#step-back").css("display", "block");
+    $("#rodape").css("display", "block");
+
+}
